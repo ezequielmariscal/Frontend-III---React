395fec9488deb4bdd09189c34f07e9202aa2904e
ezequielmariscal <ezequielramonmariscal@gmail.com>
2022-09-03 07:51:14 -0300

    Agregando entregable

--- Clase 13 - entregable/entregable-frontend-3-junio22-main/src/components/Item.js
+++ Clase 13 - entregable/entregable-frontend-3-junio22-main/src/components/Item.js
@@ -1,4 +1,4 @@
-
+import { useState } from 'react';
 // El componente Item no tiene componentes hijos.
 // ESTADO: Item debe tener un número para almacenar la cantidad de stock, la misma se la defina el padre a la hora de crearlo.
 // MÉTODOS: Item debe manejar el click de su boton para restar la cantidad en stock de sí mismo y a su vez poder aumentar el estado de su "abuelo" App.
@@ -9,11 +9,33 @@
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
 
-export default function Item() {
+
+export default function Item(props) {
+
+  const [cantidadStock, setCantidadStock] = useState(props.stock);
+
+  const restarItem = () =>{
+    if (cantidadStock >= 1) {
+      setCantidadStock(cantidadStock - 1);
+    }
+    else{
+      alert("Sin Stock") 
+    }
+  }
+  const sumarYRestar = () => {
+    restarItem();
+    props.contador();
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{props.nombre}</h3>
+      <p>{props.descripcion}</p>  
+      <h5>
+        En stock:
+        <span>{cantidadStock===0?"Agotado": cantidadStock}</span> 
+      </h5>
+      <button disabled={cantidadStock === 0} onClick={()=>sumarYRestar()}>{cantidadStock===0?"Sin stock":"Comprar"}</button>
     </div>
   )
 }
--- Clase 13 - entregable/entregable-frontend-3-junio22-main/src/components/Listado.js
+++ Clase 13 - entregable/entregable-frontend-3-junio22-main/src/components/Listado.js
@@ -1,16 +1,28 @@
 import React from 'react'
-
+import data from './data.json';
+import Item from './Item.js'; 
 // Debemos importar el listado de objetos del archivo JSON para usarlos como array a la hora de crear cada Item.
 // El componente Listado es el padre de:
-import Item from './Item' // - varios Item
+// - varios Item
 // ESTADO: Listado no necesita manejar un estado.
 // MÉTODOS: Listado no requiere de métodos.
 // PROPS: Listado recibe el método para aumentar el estado de App y se lo pasa a cada uno de sus hijos.
 
-export default function Listado() {
+export default function Listado(props) {
+
+
+
   return (
     <div className='container'>
-      {/* renderizamos los Item aquí */}
+      {data.map((elemento) => 
+      
+        <Item
+        nombre={elemento.producto.nombre}
+        descripcion={elemento.producto.descripcion}
+        stock={elemento.stock}
+        agregarCompra={props.agregarCompra}/>
+       
+      )}
     </div>
   )
 }
