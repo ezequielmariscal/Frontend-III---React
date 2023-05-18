e67563df5b92c6e43b520ae8c4b799af3cb8d650
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-18 19:04:38 -0300

    Add-Testimonios

---
+++ ProyectoPrueba/mi-app/src/components/Testimonio.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img  
+        className="imagen-testimonio"
+        src={require('../imagenes/morty.png')}
+        alt='Foto de rick'/>
+    </div>
+  )
+
+
+    
+}
